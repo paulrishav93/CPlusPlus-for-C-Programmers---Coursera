67fb982ef366d5f1b075e4d2bcb8563a1233e1f9
Rishav Paul <rishavpaul.nitw@gmail.com>
2013-11-10 08:26:59 -0800

    Create Enum Class.cpp

---
+++ Enum Class.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+enum class PlayerColor{RED,BLUE};
+
+ostream& operator<<(ostream &stream, PlayerColor &ob)
+{
+    if(ob==PlayerColor::BLUE)
+        cout<<"BLUE";
+    else if(ob==PlayerColor::RED)
+        cout<<"RED";
+
+    return stream;
+}
+
+PlayerColor operator+(PlayerColor &ob2)
+{
+    return static_cast<PlayerColor>((static_cast<int>(ob2)+1)%2);
+}
+
+int main()
+{
+    PlayerColor player1color=PlayerColor::RED;
+    PlayerColor player2color=+player1color;
+    cout<<player2color;
+}
