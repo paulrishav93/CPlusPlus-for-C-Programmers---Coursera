5ce6e0521487bd9c3e2ba54577bf23068c666549
Rishav Paul <rishavpaul.nitw@gmail.com>
2013-11-09 22:10:58 -0800

    Update Using Comparator in C++.cpp

--- Using Comparator in C++.cpp
+++ Using Comparator in C++.cpp
@@ -30,3 +30,49 @@ int main()
     for(int i=1;i<10;++i)
         cout<<v[i].age;
 }
+
+/*
+#include<iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Person
+{
+public:
+    int age;
+};
+
+class cmp
+{
+public:
+    bool operator() (  Person const &a,  Person const &b )
+    {
+        return a.age < b.age ;
+    }
+};
+
+
+//bool cmp( const Person &a, const Person &b )
+//{
+//        return a.age < b.age ;
+//}
+
+int main()
+{
+    vector <Person> v;
+
+    v.resize(10);
+
+    for(int i=1;i<10;++i)
+    {
+        v[i].age=10-i;
+    }
+    //cmp cmp; this can be initialized at first as well.
+    //sort( v.begin(), v.end(), cmp); can be used
+    sort( v.begin(), v.end(), cmp());
+
+    for(int i=1;i<10;++i)
+        cout<<v[i].age;
+}
+*/
